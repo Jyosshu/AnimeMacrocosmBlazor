71ccac8f4755e6a5d6dbbe35cef2b5310f90f73e
Josh Wygle <jwygle@gmail.com>
2020-11-05 19:21:36 -0500

    first commit

---
+++ DataAccess/SqlDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ClassLibrary
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<SqlDataAccess> _log;
+        public string ConnectionStringName { get; set; } = "AnimeMacrocom_Docker";
+
+        public SqlDataAccess(IConfiguration config, ILogger<SqlDataAccess> log)
+        {
+            _config = config;
+            _log = log;
+        }
+
+
+    }
+}
